7c76584215790e121ca416bf58f1379ea219f523
Nicholas Siebenaller <nicholassiebenaller@nicholass-mbp.home>
2018-12-30 11:21:25 -0600

    Basic Directory Funcitonality

--- src/components/Directory/directory.js
+++ src/components/Directory/directory.js
@@ -19,19 +19,39 @@ export default class Directory extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      currPage: 0,
+      perPage: 10,
     }
   }
 
 
 
   render() {
-
+    const {members} = this.props
+    const {currPage, perPage} = this.state
+    const start = currPage * perPage
+    const page = members.slice(start,start+perPage)
     return(
       <div className="main-container">
         <div className="dashboard-header">Directory</div>
-        <div className="main-contents">
+        <div className="dir-contents">
+          {
+            page.map((member, idx) => (
+              <DirCard member={member} key={`dir-mem-${idx}`} />
+            ))
+          }
         </div>
       </div>
     )
   }
 }
+
+const DirCard = ({member}) => (
+  <div className="dir-card">
+    <div>{member.first_name}</div>
+    <div>{member.last_name}</div>
+    <div>{member.address}</div>
+    <div>{member.city}</div>
+    <div>{member.state}</div>
+  </div>
+)
