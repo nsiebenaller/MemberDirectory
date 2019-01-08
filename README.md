4fa95a2d4f2cf6acb18fabecf0c6de77e08f6b28
Nicholas Siebenaller <nicholassiebenaller@nicholass-mbp.home>
2019-01-07 20:45:16 -0600

    Pagination for Directory

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ $ npm install
 
 ## Import CSV
 ```
-\copy "Members"(id, first_name, last_name, address, city, state, zip, home_phone, cell_phone, email, membership_date, status, birth_date, birth_year) from './frcc2.csv' csv header;
+\copy "Members"(id, first_name, last_name, address, city, state, zip, home_phone, cell_phone, email, membership_date, status, birth_date, birth_year) from './frccfull.csv' csv header;
 ```
 
-## Running
+## Runningems
 Build files and start local server in watch mode:
 ```js
 $ npm run dev
--- src/components/Directory/directory.js
+++ src/components/Directory/directory.js
@@ -4,10 +4,12 @@ import {
   ViewModule,
   BubbleChart,
   People,
-  Person
+  Person,
+  Add,
 } from '@material-ui/icons'
+import {Button} from '@material-ui/core'
 import {storeParam} from '../../actions/index'
-
+import Paginator from './paginator.js'
 
 @connect(
   state => ({
@@ -18,22 +20,36 @@ import {storeParam} from '../../actions/index'
 export default class Directory extends Component {
   constructor(props) {
     super(props)
+    console.log("PROPS", props)
+    const perPage = 10
+    const max = Math.floor(props.members.length / perPage)
     this.state = {
       currPage: 0,
-      perPage: 10,
+      perPage: perPage,
+      maxPage: max,
     }
   }
 
-
+  setVal = (obj) => this.setState(obj)
 
   render() {
     const {members} = this.props
-    const {currPage, perPage} = this.state
+    const {currPage, perPage, maxPage} = this.state
     const start = currPage * perPage
     const page = members.slice(start,start+perPage)
     return(
       <div className="main-container">
-        <div className="dashboard-header">Directory</div>
+        <div className="dash-top">
+          <div className="dashboard-header">Directory</div>
+          <div className="right-actions">
+            <NewMember />
+            <Paginator
+              setState={this.setVal}
+              currPage={currPage}
+              maxPage={maxPage}
+            />
+          </div>
+        </div>
         <div className="dir-contents">
           {
             page.map((member, idx) => (
@@ -48,10 +64,29 @@ export default class Directory extends Component {
 
 const DirCard = ({member}) => (
   <div className="dir-card">
-    <div>{member.first_name}</div>
     <div>{member.last_name}</div>
+    <div>{member.first_name}</div>
     <div>{member.address}</div>
     <div>{member.city}</div>
     <div>{member.state}</div>
+    <div>
+      {(member.cell_phone) ?
+        (`cell: ${member.cell_phone}`) :
+        ((member.home_phone) ?
+        (`home: ${member.home_phone}`) :
+        (`no phone`))}
+    </div>
+    <div>{member.email}</div>
   </div>
 )
+
+const NewMember = (props) => {
+  return(
+    <div className="new-member-btn">
+      <Button
+        variant="outlined"
+        color="secondary"
+      ><Add className="new-member-icon"/>create</Button>
+    </div>
+  )
+}
