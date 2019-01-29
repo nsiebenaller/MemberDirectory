f9aeb68b4ea116cf8c6f1dcdd6621f100efb4fce
nsiebenaller <nicholaspaulsiebenaller@gmail.com>
2019-01-28 21:59:18 -0600

    Prep New Member Fornm

--- public/hot/hot-update.js
+++ public/hot/hot-update.js
@@ -9,6 +9,9 @@ webpackHotUpdate(0,{
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.default = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(0);
 
@@ -16,138 +19,258 @@ var _react2 = _interopRequireDefault(_react);
 
 var _core = __webpack_require__(104);
 
-var _months = __webpack_require__(6134);
+var _months = __webpack_require__(320);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var NewMemberForm = function NewMemberForm(props) {
-  var daysOpts = [].concat(_toConsumableArray(Array(_months.months[props.birthMonth].days)));
-  return _react2.default.createElement(
-    'div',
-    { className: 'new-mem-form ' + (props.opened ? "open" : "") },
-    _react2.default.createElement(
-      'div',
-      { className: 'new-mem-card' },
-      _react2.default.createElement(
-        'h2',
-        null,
-        'Create a New Member'
-      ),
-      _react2.default.createElement(
-        'div',
-        { className: 'new-mem-contents' },
-        _react2.default.createElement(
-          'div',
-          { className: 'new-mem-label' },
-          'Member Info'
-        ),
-        _react2.default.createElement(_core.TextField, {
-          label: 'First Name',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(_core.TextField, {
-          label: 'Last Name',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(_core.TextField, {
-          label: 'Address',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(_core.TextField, {
-          label: 'City',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(_core.TextField, {
-          label: 'State',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(_core.TextField, {
-          label: 'Zip',
-          variant: 'outlined',
-          type: 'number'
-        }),
-        _react2.default.createElement(
-          'div',
-          { className: 'new-mem-label' },
-          'Contact Info'
-        ),
-        _react2.default.createElement(_core.TextField, {
-          label: 'Home Phone',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(_core.TextField, {
-          label: 'Cell Phone',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(_core.TextField, {
-          label: 'Email',
-          variant: 'outlined'
-        }),
-        _react2.default.createElement(
-          'div',
-          { className: 'new-mem-label' },
-          'Birth Date'
-        ),
-        _react2.default.createElement(
-          _core.Select,
-          {
-            value: props.birthMonth,
-            onChange: function onChange(e) {
-              return console.log(e);
-            },
-            input: _react2.default.createElement(_core.OutlinedInput, { labelWidth: 0 })
-          },
-          _months.months.map(function (obj, idx) {
-            return _react2.default.createElement(
-              _core.MenuItem,
-              { key: idx, value: idx },
-              idx + 1,
-              ' - ',
-              obj.short
-            );
-          })
-        ),
-        _react2.default.createElement(
-          _core.Select,
-          {
-            value: 1,
-            onChange: function onChange(e) {
-              return console.log(e);
-            },
-            input: _react2.default.createElement(_core.OutlinedInput, { labelWidth: 0 })
-          },
-          daysOpts.map(function (obj, idx) {
-            return _react2.default.createElement(
-              _core.MenuItem,
-              { key: 'day-' + idx, value: idx + 1 },
-              idx + 1
-            );
-          })
-        ),
-        _react2.default.createElement(_core.TextField, {
-          label: 'Year',
-          variant: 'outlined',
-          value: "",
-          onChange: function onChange() {},
-          type: 'number'
-        }),
-        _react2.default.createElement(
-          _core.Button,
-          {
-            color: 'secondary',
-            variant: 'contained',
-            onClick: function onClick() {
-              return console.log("click!");
-            }
-          },
-          'Create'
-        )
-      )
-    )
-  );
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var formDaysForMonth = function formDaysForMonth(monthIdx) {
+  var tmp = _months.months[monthIdx].days;
+  var daysOpts = Array.apply(null, Array(tmp)).map(function (x, i) {
+    return i;
+  });
+  return daysOpts;
 };
+
+var NewMemberForm = function (_Component) {
+  _inherits(NewMemberForm, _Component);
+
+  function NewMemberForm(props) {
+    _classCallCheck(this, NewMemberForm);
+
+    var _this = _possibleConstructorReturn(this, (NewMemberForm.__proto__ || Object.getPrototypeOf(NewMemberForm)).call(this, props));
+
+    _this.handleSubmit = function () {
+      var state = _this.state;
+
+      console.log("TIME TO SUBMIT", _this.state);
+      var valid = state.firstname !== "" && state.lastname !== "" && state.address !== "" && state.city !== "" && state.state !== "" && state.zip !== "";
+      if (valid) {
+        var newMember = {
+          first_name: state.firstname,
+          last_name: state.lastname,
+          address: state.address,
+          city: state.city,
+          state: state.state,
+          zip: state.zip,
+          home_phone: state.homephone,
+          cell_phone: state.cellphone,
+          email: state.email,
+          membership_date: state.membershipyear,
+          status: "member",
+          birth_date: state.birthmonth + "/" + state.birthday,
+          birth_year: state.birthyear
+        };
+        console.log("check!", newMember);
+      } else {
+        window.alert("Please Fill In All Fields!");
+      }
+    };
+
+    _this.state = {
+      firstname: "",
+      lastname: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+      homephone: "",
+      cellphone: "",
+      email: "",
+      membershipyear: "",
+      birthmonth: 0,
+      birthday: 1,
+      birthyear: ""
+    };
+    return _this;
+  }
+
+  _createClass(NewMemberForm, [{
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      var props = this.props,
+          state = this.state;
+
+      var birthDayOpts = formDaysForMonth(state.birthmonth);
+      return _react2.default.createElement(
+        'div',
+        { className: 'new-mem-form ' + (props.opened ? "open" : "") },
+        _react2.default.createElement(
+          'div',
+          { className: 'new-mem-card' },
+          _react2.default.createElement(
+            'h2',
+            null,
+            'Create a New Member'
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'new-mem-contents' },
+            _react2.default.createElement(
+              'div',
+              { className: 'new-mem-label' },
+              'Member Info'
+            ),
+            _react2.default.createElement(_core.TextField, {
+              label: 'First Name',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ firstname: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              label: 'Last Name',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ lastname: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              label: 'Address',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ address: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              label: 'City',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ city: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              label: 'State',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ state: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              label: 'Zip',
+              variant: 'outlined',
+              type: 'number',
+              onChange: function onChange(e) {
+                return _this2.setState({ zip: e.target.value });
+              }
+            }),
+            _react2.default.createElement(
+              'div',
+              { className: 'new-mem-label' },
+              'Contact Info'
+            ),
+            _react2.default.createElement(_core.TextField, {
+              label: 'Home Phone',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ homephone: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              label: 'Cell Phone',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ cellphone: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              label: 'Email',
+              variant: 'outlined',
+              onChange: function onChange(e) {
+                return _this2.setState({ email: e.target.value });
+              }
+            }),
+            _react2.default.createElement(_core.TextField, {
+              fullWidth: true,
+              label: 'Membership Year',
+              variant: 'outlined',
+              value: state.membershipyear,
+              onChange: function onChange(e) {
+                return _this2.setState({ membershipyear: e.target.value });
+              },
+              type: 'number'
+            }),
+            _react2.default.createElement(
+              'div',
+              { className: 'new-mem-label' },
+              'Birth Date'
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'date-row' },
+              _react2.default.createElement(
+                _core.Select,
+                {
+                  fullWidth: true,
+                  value: state.birthmonth,
+                  onChange: function onChange(e) {
+                    return _this2.setState({ birthmonth: e.target.value, birthday: 1 });
+                  },
+                  input: _react2.default.createElement(_core.OutlinedInput, { labelWidth: 0 })
+                },
+                _months.months.map(function (obj, idx) {
+                  return _react2.default.createElement(
+                    _core.MenuItem,
+                    { key: idx, value: idx },
+                    idx + 1,
+                    ' - ',
+                    obj.long
+                  );
+                })
+              ),
+              _react2.default.createElement(
+                _core.Select,
+                {
+                  fullWidth: true,
+                  value: state.birthday,
+                  onChange: function onChange(e) {
+                    return _this2.setState({ birthday: e.target.value });
+                  },
+                  input: _react2.default.createElement(_core.OutlinedInput, { labelWidth: 0 })
+                },
+                birthDayOpts.map(function (obj, idx) {
+                  return _react2.default.createElement(
+                    _core.MenuItem,
+                    { key: 'day-' + idx, value: idx + 1 },
+                    idx + 1
+                  );
+                })
+              ),
+              _react2.default.createElement(_core.TextField, {
+                fullWidth: true,
+                label: 'Year',
+                variant: 'outlined',
+                value: state.birthyear,
+                onChange: function onChange(e) {
+                  return _this2.setState({ birthyear: e.target.value });
+                },
+                type: 'number'
+              })
+            ),
+            _react2.default.createElement(
+              _core.Button,
+              {
+                color: 'secondary',
+                variant: 'contained',
+                onClick: this.handleSubmit
+              },
+              'Create'
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return NewMemberForm;
+}(_react.Component);
 
 exports.default = NewMemberForm;
 
--- src/components/Directory/newMember.js
+++ src/components/Directory/newMember.js
@@ -10,91 +10,176 @@ import {
 } from '@material-ui/core'
 import {months} from '../../json/months.json'
 
-const NewMemberForm = (props) => {
-  const daysOpts = [...Array(months[props.birthMonth].days)]
-  return(
-    <div className={`new-mem-form ${(props.opened) ? "open" : ""}`}>
-      <div className="new-mem-card">
-        <h2>Create a New Member</h2>
-        <div className="new-mem-contents">
-          <div className="new-mem-label">Member Info</div>
-          <TextField
-            label="First Name"
-            variant="outlined"
-          />
-          <TextField
-            label="Last Name"
-            variant="outlined"
-          />
-          <TextField
-            label="Address"
-            variant="outlined"
-          />
-          <TextField
-            label="City"
-            variant="outlined"
-          />
-          <TextField
-            label="State"
-            variant="outlined"
-          />
-          <TextField
-            label="Zip"
-            variant="outlined"
-            type="number"
-          />
-          <div className="new-mem-label">Contact Info</div>
-          <TextField
-            label="Home Phone"
-            variant="outlined"
-          />
-          <TextField
-            label="Cell Phone"
-            variant="outlined"
-          />
-          <TextField
-            label="Email"
-            variant="outlined"
-          />
-          <div className="new-mem-label">Birth Date</div>
-          <Select
-            value={props.birthMonth}
-            onChange={(e) => console.log(e)}
-            input={<OutlinedInput labelWidth={0} />}
-          >
-            {
-              months.map((obj, idx) => (
-                <MenuItem key={idx} value={idx}>{idx + 1} - {obj.short}</MenuItem>
-              ))
-            }
-          </Select>
-          <Select
-            value={1}
-            onChange={(e) => console.log(e)}
-            input={<OutlinedInput labelWidth={0} />}
-          >
-            {
-              daysOpts.map((obj, idx) => (
-                <MenuItem key={`day-${idx}`} value={idx+1}>{idx+1}</MenuItem>
-              ))
-            }
-          </Select>
-          <TextField
-            label="Year"
-            variant="outlined"
-            value={""}
-            onChange={() => {}}
-            type="number"
-          />
-          <Button
-            color="secondary"
-            variant="contained"
-            onClick={() => console.log("click!")}
-          >Create</Button>
-        </div>
-      </div>
-    </div>
-  )
+
+const formDaysForMonth = (monthIdx) => {
+  const tmp = months[monthIdx].days
+  const daysOpts = Array.apply(null, Array(tmp)).map(function (x, i) { return i; })
+  return daysOpts
 }
 
-export default NewMemberForm
+export default class NewMemberForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstname: "",
+      lastname: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+      homephone: "",
+      cellphone: "",
+      email: "",
+      membershipyear: "",
+      birthmonth: 0,
+      birthday: 1,
+      birthyear: ""
+    }
+  }
+
+  handleSubmit = () => {
+    const {state} = this
+    console.log("TIME TO SUBMIT", this.state)
+    const valid = (
+      state.firstname !== "" &&
+      state.lastname !== "" &&
+      state.address !== "" &&
+      state.city !== "" &&
+      state.state !== "" &&
+      state.zip !== ""
+    )
+    if(valid) {
+      const newMember = {
+        first_name: state.firstname,
+        last_name: state.lastname,
+        address: state.address,
+        city: state.city,
+        state: state.state,
+        zip: state.zip,
+        home_phone: state.homephone,
+        cell_phone: state.cellphone,
+        email: state.email,
+        membership_date: state.membershipyear,
+        status: "member",
+        birth_date: state.birthmonth + "/" + state.birthday,
+        birth_year: state.birthyear
+      }
+      console.log("check!", newMember)
+    }
+    else {
+      window.alert("Please Fill In All Fields!")
+    }
+
+  }
+
+  render() {
+    const {props, state} = this
+    const birthDayOpts = formDaysForMonth(state.birthmonth)
+    return(
+      <div className={`new-mem-form ${(props.opened) ? "open" : ""}`}>
+        <div className="new-mem-card">
+          <h2>Create a New Member</h2>
+          <div className="new-mem-contents">
+            <div className="new-mem-label">Member Info</div>
+            <TextField
+              label="First Name"
+              variant="outlined"
+              onChange={(e) => this.setState({firstname: e.target.value})}
+            />
+            <TextField
+              label="Last Name"
+              variant="outlined"
+              onChange={(e) => this.setState({lastname: e.target.value})}
+            />
+            <TextField
+              label="Address"
+              variant="outlined"
+              onChange={(e) => this.setState({address: e.target.value})}
+            />
+            <TextField
+              label="City"
+              variant="outlined"
+              onChange={(e) => this.setState({city: e.target.value})}
+            />
+            <TextField
+              label="State"
+              variant="outlined"
+              onChange={(e) => this.setState({state: e.target.value})}
+            />
+            <TextField
+              label="Zip"
+              variant="outlined"
+              type="number"
+              onChange={(e) => this.setState({zip: e.target.value})}
+            />
+            <div className="new-mem-label">Contact Info</div>
+            <TextField
+              label="Home Phone"
+              variant="outlined"
+              onChange={(e) => this.setState({homephone: e.target.value})}
+            />
+            <TextField
+              label="Cell Phone"
+              variant="outlined"
+              onChange={(e) => this.setState({cellphone: e.target.value})}
+            />
+            <TextField
+              label="Email"
+              variant="outlined"
+              onChange={(e) => this.setState({email: e.target.value})}
+            />
+            <TextField
+              fullWidth
+              label="Membership Year"
+              variant="outlined"
+              value={state.membershipyear}
+              onChange={(e) => this.setState({membershipyear: e.target.value})}
+              type="number"
+            />
+            <div className="new-mem-label">Birth Date</div>
+            <div className="date-row">
+              <Select
+                fullWidth
+                value={state.birthmonth}
+                onChange={(e) => this.setState({birthmonth: e.target.value, birthday: 1})}
+                input={<OutlinedInput labelWidth={0} />}
+              >
+                {
+                  months.map((obj, idx) => (
+                    <MenuItem key={idx} value={idx}>{idx + 1} - {obj.long}</MenuItem>
+                  ))
+                }
+              </Select>
+              <Select
+                fullWidth
+                value={state.birthday}
+                onChange={(e) => this.setState({birthday: e.target.value})}
+                input={<OutlinedInput labelWidth={0} />}
+              >
+                {
+                  birthDayOpts.map((obj, idx) => (
+                    <MenuItem key={`day-${idx}`} value={idx+1}>{idx+1}</MenuItem>
+                  ))
+                }
+              </Select>
+              <TextField
+                fullWidth
+                label="Year"
+                variant="outlined"
+                value={state.birthyear}
+                onChange={(e) => this.setState({birthyear: e.target.value})}
+                type="number"
+              />
+            </div>
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={this.handleSubmit}
+            >Create</Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
