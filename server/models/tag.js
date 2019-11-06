6cd5554a721665b0d98024b3b894b613dbb03580
nsiebenaller <nicholaspaulsiebenaller@gmail.com>
2019-11-05 19:43:51 -0600

    Delete Tags Route

--- server/models/tag.js
+++ server/models/tag.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Tag = sequelize.define('Tag', {
     name: DataTypes.STRING,
+    color: DataTypes.STRING,
   }, {});
   Tag.associate = function(models) {
     Tag.belongsToMany(models.Member, { as: 'Member', through: 'MemberTags', foreignKey: 'TagId', otherKey: 'MemberId'})
--- server/routes/tags.js
+++ server/routes/tags.js
@@ -13,6 +13,14 @@ router.route('/new')
 
   })
 
+router.route('/delete')
+  .post((req, res, next) => {
+    const { tagID } = req.body;
+    db.Tag.destroy({ where: { id: tagID } })
+      .then((x) => res.status(200).send({ success: true }))
+      .catch((x) => res.status(200).send({ success: false }))
+  })
+
 router.route('/')
   .get(async(req, res, next) => {
     const allTags = await db.Tag.findAll()
